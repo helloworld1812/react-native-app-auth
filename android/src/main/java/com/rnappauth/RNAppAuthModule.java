e29bcbf1dc209a1516f0bb181b7ccd2ff19ce245
rt2zz <zack@root-two.com>
2019-02-26 15:19:51 -0800

    --amend

--- android/src/main/java/com/rnappauth/RNAppAuthModule.java
+++ android/src/main/java/com/rnappauth/RNAppAuthModule.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
